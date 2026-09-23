64a4a9bb57012609ab6feaaedcd4c4ca4fa68682
Álvaro Mozo Gaspar <ciencias8000@gmail.com>
2026-09-23 16:36:42 +0200

    Añadido

--- hola1.py
+++ hola1.py
@@ -1,7 +1,47 @@
+"""
 diccionario = {
     "nombre" : "Pepe",
     "apellido" : "López",
     "edad" : 18
 }
 
+diccionario["edad"] = 20
+diccionario["direccion"] = "Calle 1"
+del diccionario["edad"]
+print(diccionario["direccion"])
 print(diccionario)
+
+lista = [1,2,3]
+estudiante = {
+    "nombre" : "Joselín",
+    "apellido" : "Flores",
+    "modulos" : ["Acceso a datos", "Python", "Proyecto"]
+}
+print(estudiante["modulos"][0])
+"""
+"""
+estudiantes = [
+    {
+        "nombre" : "Joselín",
+        "apellido" : "Flores",
+        "modulos" : ["Acceso a datos", "Python", "Proyecto"]
+    },
+        {
+        "nombre" : "Camilo",
+        "apellido" : "Sesto",
+        "modulos" : ["Acceso a datos", "Desarrollo de interfaces"]
+    }
+]
+print(estudiantes[0]["modulos"][0])
+"""
+
+conjunto = {1, 2, 3, 3, 4}
+conjunto.add(7)
+conjunto.remove(3)
+print(conjunto)
+
+def main():
+    print("ESTE ES EL MAIN")
+
+if __name__ == "__main__":
+    main()
